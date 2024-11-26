471ead5fdc7c58c188eb6ea1fb1fa552df44c716
dizzlesticks <sodha.dhiran@gmail.com>
2024-11-26 10:35:32 +0000

    complete 7 post comment by article id

--- models/app.models.js
+++ models/app.models.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const db = require("../db/connection");
+const format = require("pg-format");
 
 exports.getEndpointsFromFile = () => {
   return fs.readFile(`${__dirname}/../endpoints.json`).then((contents) => {
@@ -34,27 +35,40 @@ exports.getArticlesFromDatabase = () => {
   GROUP BY articles.article_id
   ORDER BY articles.created_at DESC`;
   return db.query(query).then(({ rows }) => {
-    return rows
-    // return rows.map((article) => {
-    //   article.comment_count = Number(article.comment_count);
-    //   return article;
-    // });
+    return rows.map((article) => {
+      article.created_at = String(article.created_at);
+      return article;
+    });
   });
 };
 
 exports.getCommentsFromDatabase = (article_id) => {
-  if (article_id && !/^\d+$/.test(article_id))
-    return Promise.reject({ msg: "invalid article id", status: 400 });
   const values = [];
   let query = `SELECT * FROM comments`;
   if (article_id) {
     query += ` WHERE article_id = $1`;
     values[0] = article_id;
   }
-  query += ` ORDER BY created_at DESC`
+  query += ` ORDER BY created_at DESC`;
   return db.query(query, values).then(({ rows }) => {
     if (rows.length === 0)
       return Promise.reject({ msg: "article id not found", status: 404 });
-    else return rows;
+    else
+      return rows.map((comment) => {
+        comment.created_at = String(comment.created_at);
+        return comment;
+      });
+  });
+};
+
+exports.addCommentToDatabase = (article_id, username, body) => {
+  const array = [[article_id, username, body]];
+  const query = `INSERT INTO comments(article_id, author, body) 
+  VALUES %L
+  RETURNING *`;
+  const formattedQuery = format(query, array);
+  return db.query(formattedQuery).then(({ rows }) => {
+    rows[0].created_at = String(rows[0].created_at);
+    return rows[0];
   });
 };
